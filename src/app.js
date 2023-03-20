10393e8c6641708a8d689d3ba96a8b7586d614ea
Alexandre Trieste <alexandre.trieste@gmail.com>
2023-03-20 03:25:17 -0300

    modification elements

--- src/app.js
+++ src/app.js
@@ -2,15 +2,16 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
-  
+
   const number = document.getElementById("number").value;
-  
+  const message = "Olá";
+
   // Formatar o número de telefone para o padrão internacional
-  const formattedNumber = `https://api.whatsapp.com/send?phone=${number.replace(/\D/g, "")}&text=Olá`;
-  
+  const formattedNumber = `https://api.whatsapp.com/send?phone=55${number.replace(/\D/g, "")}&text=${encodeURIComponent(message)}`;
+
   // Abrir a janela do WhatsApp com a mensagem
   window.open(formattedNumber, "_blank");
-  
+
   // Limpar o formulário
   document.getElementById("number").value = "";
 });
