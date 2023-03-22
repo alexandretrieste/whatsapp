91aac56798f0c278e7d896edaf91bdef3a15e93d
Alexandre Trieste <alexandre.trieste@gmail.com>
2023-03-22 17:57:04 -0300

    fix: new estructure, angular structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'whats';
+  number: string = "";
+
+  onSubmit() {
+    const message = "Olá mundo";
+
+    // Formatar o número de telefone para o padrão internacional
+    const formattedNumber = `https://api.whatsapp.com/send?phone=55${this.number.replace(/\D/g, "")}&text=${encodeURIComponent(message)}`;
+
+    // Abrir a janela do WhatsApp com a mensagem
+    window.open(formattedNumber, "_blank");
+
+    // Limpar o formulário
+    this.number = "";
+  }
 }
